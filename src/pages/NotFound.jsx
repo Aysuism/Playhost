c0a73f8c65cb93a5bd716b665221e382a27b14a5
Aysuism <iaysu1810@gmail.com>
2025-04-30 01:01:01 +0400

    commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <section className='not-found-page'>
       <div className="text">
-        <img src='../../public/glitch-404-removebg-preview.png' alt="" />
+        <img src='/glitch-404-removebg-preview.png' alt="" />
         <h3>OOPS... PAGE NOT FOUND</h3>
         <p>Try Using The Button Below To Go To Main Page Of The Site</p>
         <Link className='back-home' to={'/'}>Back to HomePage</Link>
